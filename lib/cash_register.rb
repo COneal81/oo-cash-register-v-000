56acd084cae7b220efbed8fa4605b7acf605cd7b
Kari O'Neal <triadfirewood@gmail.com>
2019-02-26 18:55:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,44 +1,53 @@
+
 class CashRegister
-  
-  attr_accessor :total, :discount, :quantity, :price
-  
-  def initialize(discount = 0)
+  attr_accessor  :items, :price, :discount, :total, :last_transaction
+
+    @@items = []
+    @@all = []
+
+  def initialize(discount = 0 )
     @total = 0
-    @discount = discount
-    @quantity = quantity
     @price = price
+    @discount = discount
     @items = []
-    @transactions = []
+    @@items << self
+    @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@items.each do |v|
+    puts "#{v.items}"
+    puts "#{v.price}"
+    end
+  end
+
+#accepts a title and a price and increases the total
+#also accepts an optional quantity
+#doesn't forget about the previous total
   def add_item(title, price, quantity = 1)
-    @total += price * quantity
-    @transactions << price
-    i = quantity
-    until i == 0 do
-    @items << title
-    i -= 1 
-    end
+    self.total += price * quantity 
+   quantity.times do 
+      items << total
+      end
+     self.last_transaction = price * quantity 
   end
-  
+
+
   def apply_discount
-    if @discount == 0 
-      return "There is no discount to apply." 
+    if discount == 0
+      puts "You do not have a discount"
     else
-    self.total -= (0.01 * @discount * @total).to_i
-      return "After the discount, the total comes to $#{self.total}."
-      @total
-    end
+      self.total = (total * ((100 - discount.to_f / 100)).to_i
+      puts "After your discount, your total is $#{total}"
+    
   end
   
-  def items
-    @items 
-  end
-
   def void_last_transaction
-    self.total = @total - @transactions.pop
-  end 
-end    
-
-
-  
+  #subtracts the last transaction from the total
+    self.total = @total -= last_transaction
+  end
+end
